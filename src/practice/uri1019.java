09a853a90a5ce059fc6e2321408fe1a137b49a80
TheTimeBug <nasim15-10463@diu.edu.bd>
2022-02-14 19:31:37 +0600

    1019

---
+++ src/practice/uri1019.java
@@ -0,0 +1,29 @@
+package practice;
+import java.util.Scanner; //used to take input form user
+//import java.text.DecimalFormat; //used as number formatter
+//import java.lang.Math; //import for java  math operation
+public class uri1019 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int time= input.nextInt();
+		input.close();
+		
+		int hour=0,minute=0,second=0;
+		
+		if(time>3600) {
+			hour = time/3600;
+			time = time%3600;
+		}
+		 if(time>60) {
+			minute = time/60;
+			time = time%60;
+		}
+		
+		System.out.println(hour+":"+minute+":"+time);
+		
+
+	}
+
+}
