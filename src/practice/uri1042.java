35f49df296b9d37741010b1c6a35bd77db33e40b
TheTimeBug <nasim15-10463@diu.edu.bd>
2022-02-15 18:33:56 +0600

    1042

---
+++ src/practice/uri1042.java
@@ -0,0 +1,40 @@
+package practice;
+import java.util.Scanner;
+public class uri1042 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int a,b,c;
+		a = input.nextInt();
+		b = input.nextInt();
+		c = input.nextInt();
+		input.close();
+		
+		int h1=a,h2=b,h3=c,temp=0;
+		
+		if(h1>=h2) {
+		}else {
+			temp=h1;
+			h1=h2;
+			h2=temp;
+		}
+		if(h1>=h3) {
+		}else {
+			temp=h1;
+			h1=h3;
+			h3=temp;
+		}
+		if(h2>=h3) {
+		}else {
+			temp=h2;
+			h2=h3;
+			h3=temp;
+		}
+		
+		System.out.println(h1+"\n"+h2+"\n"+h3+"\n");
+		System.out.println(a+"\n"+b+"\n"+c);
+
+	}
+
+}
