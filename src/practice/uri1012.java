3d15854663e9bdd86ab8e7fc86882193686ce778
TheTimeBug <nasim15-10463@diu.edu.bd>
2022-02-17 13:40:38 +0600

    1012

--- src/practice/uri1012.java
+++ src/practice/uri1012.java
@@ -1,12 +1,32 @@
 package practice;
-
-
+import java.util.Scanner;
+import java.text.DecimalFormat; 
 public class uri1012 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double a,b,c,TRIANGULO,CIRCULO,TRAPEZIO,QUADRADO,RETANGULO;
+		double pi=3.14159;
+		a= input.nextDouble();
+		b= input.nextDouble();
+		c= input.nextDouble();
+		input.close();
+		TRIANGULO=(double)0.5*a*c;
+		CIRCULO=pi*(c*c);
+		TRAPEZIO=((a+b)/2)*c;
+		QUADRADO=b*b;
+		RETANGULO=a*b;
 		
-		int number1 = 10;
-		int number2 = 2;
+		DecimalFormat formatter = new DecimalFormat("#0.000");
+		System.out.println("TRIANGULO: "+formatter.format(TRIANGULO));
+		System.out.println("CIRCULO: "+formatter.format(CIRCULO));
+		System.out.println("TRAPEZIO: "+formatter.format(TRAPEZIO));
+		System.out.println("QUADRADO: "+formatter.format(QUADRADO));
+		System.out.println("RETANGULO: "+formatter.format(RETANGULO));
+		
+		
+		
+		
 				
 		
 
